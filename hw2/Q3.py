ad0e26e7056bbf0020e4648307f167e735b91b51
s-elo <zz321yy@qq.com>
2022-02-15 13:16:48 +0800

    Q3 single layer perceptron for classification without data normalization

--- hw2/Q3.py
+++ hw2/Q3.py
@@ -1,6 +1,10 @@
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from slp import slp_model
 
 datasetPath = './dataset/'
 
@@ -39,8 +43,86 @@ def loadData(train_num=450, test_num=50):
     test_data, test_labels = shuffle(
         np.array(test_data), np.array(test_labels), random_state=0)
 
-    return train_data, train_labels.reshape(1, train_labels.shape[0]), test_data, test_labels.reshape(1, test_labels.shape[0])
+    # make them in [0, 1]
+    return train_data / 255, train_labels, test_data / 255, test_labels
 
 
 train_x, train_y, test_x, test_y = loadData()
 print(train_x.shape, test_y.shape)
+
+
+def classify(opts={}):
+    '''
+    Default setting:
+        learning rate: 0.001
+        activation: relu
+        solver: adam
+        batch size: 200
+        hidden_size: (100,)
+        max_iter: 1000
+    x: (sample, feature)
+    y: (sample, 1)
+    '''
+    mlp = MLPClassifier(random_state=1, max_iter=1, **opts)
+    params = mlp.get_params()
+
+    epochs = 300
+
+    train_acc = []
+    test_acc = []
+
+    for i in range(0, epochs):
+        mlp.set_params(**params)
+
+        # Update the model with a single iteration over the given data
+        mlp.partial_fit(train_x, train_y, [1, 0])
+
+        # set the params for next iteration
+        params = mlp.get_params()
+
+        # get training set accuracy
+        prediction = mlp.predict(train_x)
+        train_acc.append(accuracy_score(train_y, prediction))
+
+        # get test set accuracy
+        prediction = mlp.predict(test_x)
+        test_acc.append(accuracy_score(test_y, prediction))
+
+    # display the final accuracy
+    print(f'training set accuracy: {train_acc[len(train_acc) - 1]}')
+    print(f'test set accuracy: {test_acc[len(test_acc) - 1]}')
+
+    plt.figure()
+    plt.plot(range(0, epochs), train_acc)
+    plt.plot(range(0, epochs), test_acc)
+    plt.legend(['train_acc', 'test_acc'])
+
+    plt.figure()
+    plt.plot(mlp.loss_curve_)
+
+
+slp = {
+    'hidden_layer_sizes': (0, )
+}
+
+# a. single layer perceptron
+# classify(slp)
+
+
+def slp(train_x, train_y, test_x, test_y):
+    train_acc, test_acc = slp_model(train_x, train_y, test_x, test_y,
+                                    iteration_number=2000, learning_rate=0.05, print_cost=False)
+
+    max_test_acc = max(test_acc)
+    print(f'maximum test accuracy: {max_test_acc}')
+
+    plt.figure()
+    plt.plot(range(0, 2000), train_acc)
+    plt.plot(range(0, 2000), test_acc)
+    plt.legend(['train_acc', 'test_acc'])
+
+
+slp(train_x.T, train_y.reshape(
+    1, train_y.shape[0]), test_x.T, test_y.reshape(1, test_y.shape[0]))
+
+plt.show()
---
+++ hw2/slp.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def predict(w, b, X):
+    sample_number = X.shape[1]
+
+    Y_predict = np.zeros((1, sample_number))
+
+    # the output
+    A = sigmoid(np.dot(w.T, X) + b)  # (1, sample_number)
+
+    for i in range(A.shape[1]):
+        Y_predict[0, i] = 1 if A[0, i] > 0.5 else 0
+
+    assert(Y_predict.shape == (1, sample_number))
+
+    return Y_predict
+
+
+def get_acc(prediction, label):
+    return 100 - np.mean(np.abs(prediction - label)) * 100
+
+
+def init_with_zeros(dimensions):
+    w = np.zeros(shape=(dimensions, 1))
+
+    b = 0
+
+    assert(w.shape == (dimensions, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+
+    return (w, b)
+
+
+def iteration(w, b, X, Y):
+    """
+    w - weights: (num_px * num_px * 3, 1)
+
+    b - bias
+
+    X - training input: (num_px * num_px * 3, sample_number)
+
+    Y - labels: (1, sample_number)
+    """
+
+    # forward propagation
+    sample_number = X.shape[1]
+
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = (-1 / sample_number) * np.sum(Y *
+                                         np.log(A) + (1 - Y) * np.log(1 - A))
+
+    # back propagation
+    dw = (1 / sample_number) * np.dot(X, (A - Y).T)
+    db = (1 / sample_number) * np.sum(A - Y)
+
+    assert(w.shape == dw.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {
+        'dw': dw,
+        'db': db
+    }
+
+    return (grads, cost)
+
+
+def optimize(w, b, X, Y, tx, ty, iteration_number, learning_rate, print_cost=False):
+    costs = []
+    train_acc = []
+    test_acc = []
+
+    for i in range(iteration_number):
+        grads, cost = iteration(w, b, X, Y)
+
+        dw = grads['dw']
+        db = grads['db']
+
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        train_acc.append(get_acc(predict(w, b, X), Y) / 100)
+        test_acc.append(get_acc(predict(w, b, tx), ty) / 100)
+
+        # record the cost every 100 iteration
+        if (i % 100 == 0):
+            costs.append(cost)
+
+        if ((print_cost) and (i % 100 == 0)):
+            print('iteration number: %i, wrong rate: %f' % (i, cost))
+
+    params = {
+        'w': w,
+        'b': b
+    }
+
+    grads = {
+        'dw': dw,
+        'db': db
+    }
+
+    return (params, grads, costs, train_acc, test_acc)
+
+
+def slp_model(train_data, train_label, test_data, test_label, iteration_number, learning_rate, print_cost=False):
+    w, b = init_with_zeros(train_data.shape[0])
+
+    params, grads, costs, train_acc, test_acc = optimize(
+        w, b, train_data, train_label, test_data, test_label, iteration_number, learning_rate, print_cost)
+
+    # optimized params
+    w = params['w']
+    b = params['b']
+
+    train_a = get_acc(predict(w, b, train_data), train_label)
+
+    test_a = get_acc(predict(w, b, test_data), test_label)
+
+    # get the accuracy
+    print(f"training data accuracy: {train_a}%")
+    print(f"testing data accuracy: {test_a}%")
+
+    return (train_acc, test_acc)
